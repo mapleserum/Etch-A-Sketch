6f481ba2d0cc0bd41b5434a68d6213230f5041a1
Dan <mapleserum@gmail.com>
2022-05-06 21:11:23 -0400

    Fix scale to work. Begin buttons.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let etch = document.querySelectorAll('.etch');
 let scale = document.querySelector('.scale')
 var container = document.querySelector(".container")
 
+
 //Events
 scale.addEventListener('click', function(e){
   let number = prompt('What size grid would you like to create?')
@@ -21,6 +22,17 @@ let refresh = document.querySelector('.refresh')
 refresh.addEventListener('click', function(e){
   window.location.reload()
 })
+
+//Color Buttons
+let black = document.querySelector('#black')
+black.addEventListener('click', function(e){
+  document.getElementById("myDiv").style.backgroundColor = 'black'
+})
+
+let white = document.querySelector('#white')
+refresh.addEventListener('click', function(e){
+  document.getElementById("myDiv").style.gridTemplateColumns = 'white'
+})
   
 //Multiplier Function
 function containSize(number){
@@ -31,5 +43,14 @@ function containSize(number){
 
   for (var i = 1; i <= (number * number); i++) {
     container.innerHTML += '<div class="etch"></div>';
- }  
+ }
+
+ document.getElementById("myDiv").style.gridTemplateColumns = "repeat(" + number + ", 1fr)"
+
+ let etch = document.querySelectorAll('.etch');
+ etch.forEach((etch) => {
+  etch.addEventListener("mouseover", function( event ) {
+    etch.classList.add('highlight')},
+    false);
+  })
 }
